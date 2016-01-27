9f1e95eb9bb20c2c67b1748bfd93f3f0b4eb445e
vidklopcic <klopcic.vid@gmail.com>
2016-01-27 20:56:49 +0100

    Added CitiSense to INSTALLED_APPS and fixed all typos in models.py for CitiSense

--- CitiSense/models.py
+++ CitiSense/models.py
@@ -4,13 +4,15 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Sensor(models.Model):
     name = models.CharField(max_length=20)
     date_added = models.DateTimeField()
+    lat = models.FloatField()
+    lng = models.FloatField()
 
 class Pollutant(models.Model):
     sensor = models.ForeignKey(Sensor)
-    short_name = models.CharField(unique=True)
-    long_name = models.CharField(unique=True)
-    description = models.CharField()
-    unit = models.CharField()
+    short_name = models.CharField(max_length=5, unique=True)
+    long_name = models.CharField(max_length=20, unique=True)
+    description = models.CharField(max_length=200)
+    unit = models.CharField(max_length=10)
 
 class Measurement(models.Model):
     date_time = models.DateTimeField()
